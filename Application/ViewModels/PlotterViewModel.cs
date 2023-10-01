ee60c65717062fd2384c4f11bdf171277c3ba212
m653l <sebastian.kura@pm.me>
2023-10-01 08:52:05 +0200

    Fix update

--- Application/ViewModels/PlotterViewModel.cs
+++ Application/ViewModels/PlotterViewModel.cs
@@ -45,10 +45,14 @@ namespace Application.ViewModels
             get => _applicationDataStore.Years;
         }
 
-        public List<ISeries> SaldosSeries { get; set; }
-        public List<ISeries> SaldosPerStudentSeries { get; set; }
-        public List<ISeries> StalinSeries { get; set; }
-        public List<ISeries> CostPerStalinSeries { get; set; }
+        [ObservableProperty]
+        public List<ISeries> saldosSeries;
+        [ObservableProperty]
+        public List<ISeries> saldosPerStudentSeries;
+        [ObservableProperty]
+        public List<ISeries> stalinSeries;
+        [ObservableProperty]
+        public List<ISeries> costPerStalinSeries;
 
         SolidColorPaint[] _paints;
 
@@ -155,47 +159,72 @@ namespace Application.ViewModels
         {
             XAxes[0].Labels = _applicationDataStore.ActiveYear.Schools.Select(i => i.Name).ToList();
 
-            var saldos = new List<decimal>();
-            var saldosPerStudent = new List<decimal>();
-            var stalin = new List<decimal>();
-            var costPerStalin = new List<decimal>();
+            var saldos = new List<PilotInfo>();
+            var saldosPerStudent = new List<PilotInfo>();
+            var stalin = new List<PilotInfo>();
+            var costPerStalin = new List<PilotInfo>();
+
+            for (int i = 0; i < _applicationDataStore.ActiveYear.Schools.Count; i++)
+                _paints = Enumerable.Range(0, _applicationDataStore.ActiveYear.Schools.Count)
+                        .Select(i => new SolidColorPaint(ColorPalletes.MaterialDesign500[i % ColorPalletes.MaterialDesign500.Count()].AsSKColor()))
+                        .ToArray();
 
             for (int i = 0; i < _applicationDataStore.ActiveYear.Schools.Count; i++)
             {
-                saldos.Add(_applicationDataStore.ActiveYear.Schools[i].GetSaldo());
-                saldosPerStudent.Add(_applicationDataStore.ActiveYear.Schools[i].GetSaldoPerStudent());
-                stalin.Add(_applicationDataStore.ActiveYear.Schools[i].GetTrzyStaliny());
-                costPerStalin.Add(_applicationDataStore.ActiveYear.Schools[i].GetCostPerStanin());
+                saldos.Add(new PilotInfo(_applicationDataStore.ActiveYear.Schools[i].Name, (double)_applicationDataStore.ActiveYear.Schools[i].GetSaldo(), _paints[i]));
+                saldosPerStudent.Add(new PilotInfo(_applicationDataStore.ActiveYear.Schools[i].Name, (double)_applicationDataStore.ActiveYear.Schools[i].GetSaldoPerStudent(), _paints[i]));
+                stalin.Add(new PilotInfo(_applicationDataStore.ActiveYear.Schools[i].Name, (double)_applicationDataStore.ActiveYear.Schools[i].GetTrzyStaliny(), _paints[i]));
+                costPerStalin.Add(new PilotInfo(_applicationDataStore.ActiveYear.Schools[i].Name, (double)_applicationDataStore.ActiveYear.Schools[i].GetCostPerStanin(), _paints[i]));
             }
 
-            var columnSeries1 = new ColumnSeries<decimal>
+            var columnSeries1 = new ColumnSeries<PilotInfo>
             {
                 Values = saldos,
                 Stroke = null,
                 Padding = 2,
 
-            };
+            }.OnPointMeasured(point =>
+            {
+                // assign a different color to each point
+                if (point.Visual is null) return;
+                point.Visual.Fill = point.Model!.Paint;
+            });
 
-            var columnSeries2 = new ColumnSeries<decimal>
+            var columnSeries2 = new ColumnSeries<PilotInfo>
             {
                 Values = saldosPerStudent,
                 Stroke = null,
                 Padding = 2
-            };
+            }.OnPointMeasured(point =>
+            {
+                // assign a different color to each point
+                if (point.Visual is null) return;
+                point.Visual.Fill = point.Model!.Paint;
+            });
 
-            var columnSeries3 = new ColumnSeries<decimal>
+            var columnSeries3 = new ColumnSeries<PilotInfo>
             {
                 Values = stalin,
                 Stroke = null,
                 Padding = 2
-            };
+            }.OnPointMeasured(point =>
+            {
+                // assign a different color to each point
+                if (point.Visual is null) return;
+                point.Visual.Fill = point.Model!.Paint;
+            });
 
-            var columnSeries4 = new ColumnSeries<decimal>
+            var columnSeries4 = new ColumnSeries<PilotInfo>
             {
                 Values = costPerStalin,
                 Stroke = null,
                 Padding = 2
-            };
+            }.OnPointMeasured(point =>
+            {
+                // assign a different color to each point
+                if (point.Visual is null) return;
+                point.Visual.Fill = point.Model!.Paint;
+            });
 
             SaldosSeries = new List<ISeries> { columnSeries1 };
             SaldosPerStudentSeries = new List<ISeries> { columnSeries2 };
